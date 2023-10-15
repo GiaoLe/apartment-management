826df8e6757491f47744c6059a40084c505553fb
GiaoLe <giaolexuan104@gmail.com>
2023-10-15 19:32:00 +0700

    Hibernate Validation fixed - with regard for simplecoding.net

--- src/main/java/com/example/demo/Apartment.java
+++ src/main/java/com/example/demo/Apartment.java
@@ -2,13 +2,12 @@ package com.example.demo;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.NotNull;
 
 
 @Data
