cb76c1b7938ba8d1792cce06c55bc23948484aaf
GiaoLe <giaolexuan104@gmail.com>
2023-10-26 20:21:00 +0700

    Clean up the code in DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.gui.MenuView;
 import com.example.demo.gui.Scene;
 import com.example.demo.gui.SceneManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.Objects;
 
 public class DemoApplication extends Application {
     public static void main(String[] args) {
@@ -18,16 +12,6 @@ public class DemoApplication extends Application {
 
     @Override
     public void start(Stage stage) {
-//        try {
-//            stage.setScene(new javafx.scene.Scene(FXMLLoader.load(Objects.requireNonNull(DemoApplication.class.getResource(MenuView.MENU.getFileName())))));
-//            stage.setTitle("Apartment Manager");
-//            stage.getIcons().add(new Image(String.valueOf(DemoApplication.class.getResource("/media/apartment-icon.png"))));
-//            stage.centerOnScreen();
-//            stage.setMaximized(true);
-//            stage.show();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
         SceneManager.setStage(stage);
         SceneManager.switchScene(Scene.LOGIN_FORM);
     }
