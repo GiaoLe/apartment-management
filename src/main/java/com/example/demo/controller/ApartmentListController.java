1e9bfb961f3f61e6d8f0300e57ec4b58e3f75dfb
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2023-10-26 21:58:11 +0700

    Chang apartment list

--- src/main/java/com/example/demo/controller/ApartmentListController.java
+++ src/main/java/com/example/demo/controller/ApartmentListController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.HibernateUtility;
 import com.example.demo.dao.Apartment;
 import com.example.demo.gui.Scene;
 import com.example.demo.gui.SceneManager;
@@ -7,44 +8,74 @@ import com.example.demo.repository.ApartmentRepository;
 import com.example.demo.service.ApartmentService;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApartmentListController {
 
-    public TableView<Apartment> apartmentTableView;
-    public TableColumn<Apartment, String> numberTableColumn;
-    public TableColumn<Apartment, Double> areaTableColumn;
-    public TableColumn<Apartment, String> totalRoomsTableColumn;
-    public TableColumn<Apartment, Integer> floorTableColumn;
-    public TableColumn<Apartment, Integer> totalResidentsTableColumn;
-    public Button newButton;
+
+    public TableColumn<?, ?> actionsColumn;
+
+
+    public TableView<String> apartmentTableView;
+
+
+    public TableColumn<String, String> availableColumn;
+
+
     public Button deleteButton;
 
+
+    public TableColumn<String, String> floorColumn;
+
+
+    public TableColumn<String, String> nAvailableColumn;
+
+
+    public Button newButton;
+
+
+    public TableColumn<String, String> occupiedColumn;
+
+    public TableColumn<String, String> residentsColumn;
+
+
+    public TableColumn<String, String> totalColumn;
     private final ApartmentService apartmentService = new ApartmentService(new ApartmentRepository());
+    private final Map<String, String> floorDetails = new HashMap<>();
+    public void updateFloorDetails() {
+        floorDetails.put("totalApartments", HibernateUtility.getSessionFactory().fromTransaction(session -> session.createQuery("select count(*) from Apartment ", Long.class)
+                .uniqueResult()).toString());
+        floorDetails.put("totalResidents", HibernateUtility.getSessionFactory().fromTransaction(session -> session.createQuery("select sum(number) from Apartment ", Long.class)
+                .uniqueResult()).toString());
+      //continue
+    }
+    public void showFloorList() {
 
 
-    @FXML
+    }
     public void initialize() {
-        apartmentTableView.setItems(FXCollections.observableList(apartmentService.findAll()));
-        numberTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getNumber()));
-        areaTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getArea()));
-        totalRoomsTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getRoomCount()).asString());
-        floorTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getFloor()));
-        totalResidentsTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getTotalResidents()));
+//        apartmentTableView.setItems(FXCollections.observableList(apartmentService.findAll()));
+//        numberTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getNumber()));
+//        areaTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getArea()));
+//        totalRoomsTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getRoomCount()).asString());
+//        floorTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getFloor()));
+//        totalResidentsTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getTotalResidents()));
+        showFloorList();
     }
 
     public void newButtonOnAction() {
         SceneManager.switchScene(Scene.APARTMENT_FORM.getFileName());
     }
 
-    public void deleteButtonOnAction() {
-        Apartment apartment = apartmentTableView.getSelectionModel().getSelectedItem();
-        if (apartment != null) {
-            apartmentTableView.getItems().remove(apartment);
-            apartmentService.remove(apartment);
-        }
+    public void deleteButtonOnAction(ActionEvent actionEvent) {
     }
 }
